cb86a6768b90e926ed2e404fb578c3933852cd47
Daniel Fang <138417504+qkfang@users.noreply.github.com>
2024-09-14 22:41:33 +1000

    Update maze-game.py

--- other/maze-game.py
+++ other/maze-game.py
@@ -3,7 +3,7 @@ on the map, text X is obstacles and you can't move into it.
 S is the starting point. 
 E is the exit. 
 
-Can you provide step by step solution?
+Can you provide step by step solution below?
 
 XXXXXXXXXXXXX
 X     X     X
